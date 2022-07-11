d3a593b997582c32e486d5ca88d3595b26adefd7
Chanon Srithongsookw <ninenote@yahoo.com>
2022-07-11 21:31:31 +0700

    Sample v3 color scheme

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const htmlContent = `
 <style>
 h1 {
   padding: 2rem;
-  background-color: #E85D75;
-  color: #F5FBFE;
+  background-color: #4B7F52;
+  color: #96E8BC;
   border:1px solid #bbb;
   text-align: center;
 }
 </style>
 </head>
 <body>
-  <h1>Sample node http server V2</h1>
+  <h1>Sample node http server V3</h1>
 </body>
 </html>
 `;
